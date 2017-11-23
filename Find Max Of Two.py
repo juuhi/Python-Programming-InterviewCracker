414c30aa4a25b7ce7ebcd49adbd7fb59ede7156c
juhis <juhisharma21@gmail.com>
2017-11-22 22:58:13 -0500

    Inserted the comments

--- Find Max Of Two.py
+++ Find Max Of Two.py
@@ -1,3 +1,6 @@
+#Define a function max() that takes two numbers as arguments and returns the largest of
+# them. Use the if-then- else construct available in Python.
+
 def max(a,b):
     if a >b:
         return a
