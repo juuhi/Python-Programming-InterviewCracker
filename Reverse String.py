6845ca6e74ed545cad9692662c246bc1405cd9a3
juhis <juhisharma21@gmail.com>
2017-12-07 15:24:42 -0500

    Exercise 6

---
+++ Reverse String.py
@@ -0,0 +1,9 @@
+# Define a function reverse() that computes the reversal of a string.
+# For example, reverse("I am testing") should return the string "gnitset ma I".
+
+def reverse(string):
+    return string[::-1]
+
+print('Enter the string')
+string = input()
+print(reverse(string))
