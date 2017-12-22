47fdbb2a6db8e43bbc4e8c3264345a16f3eab37d
juhis <juhisharma21@gmail.com>
2017-12-21 20:41:56 -0500

    Practice Session

---
+++ Count_words.py
@@ -0,0 +1,10 @@
+"""juhiNewYork"""
+
+def count_words(string):
+    total = 1
+    for i in string:
+        if i.isupper():
+            total += 1
+    return total
+
+print(count_words('IloveFlorida'))
