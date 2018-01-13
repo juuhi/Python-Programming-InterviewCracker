97e7b62b90955a83f1738ec5071cf1d6391107bf
juhis <juhisharma21@gmail.com>
2018-01-13 17:40:10 -0500

    Practice Session

--- Continue.py
+++ Continue.py
@@ -1,8 +1,16 @@
-# Program to show the use of continue statement inside loops
+# Program to show the use of continue and break keyword inside loops and their difference
 
 for val in "string":
     if val == "i":
         continue    #(to continue to the loop)
+    print(val)
+
+print("The end")
+
+# Use of Break Keyword
+for val in "string":
+    if val == "i":
+        break    #(to come out of the loop)
     print(val)
 
 print("The end")
