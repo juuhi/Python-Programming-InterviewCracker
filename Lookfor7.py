bf197fe408a228318305775a6e02b4bb610395ff
juhis <juhisharma21@gmail.com>
2018-01-13 17:29:23 -0500

    Practice Session

--- Lookfor7.py
+++ Lookfor7.py
@@ -1,28 +1,32 @@
-# #[2,3,4,5,6,13,12,7,2] -> 16
-#
-# def skip67(nums):
-#     total = 0
-#     i = 0
-#     flag = True
-#     while i<len(nums):
-#         if nums[i] == 6:
-#             while nums[i]!=7:
-#                 i=i+1
-#             i=i+1
-#         else:
-#             total = total+nums[i]
-#             i=i+1
-#     return total
-#
-# print skip67([2,3,4,5,6,13,12,7,2])
-# print skip67([1, 2, 2])
-# print skip67([1, 2, 2, 6, 99, 99, 7])
-# print skip67([1, 1, 6, 7, 2])
-#         #     flag = False
-#         #     i = i+1
-#         #     if flag =
-#         # else:
-#         #     total=total+nums[i]
+"""Return the sum of the numbers in the array,
+except ignore sections of numbers starting with a 6 and
+extending to the next 7 (every 6 will be followed by at least one 7).
+Return 0 for no numbers."""
+# sum67([1, 2, 2]) → 5
+# sum67([1, 2, 2, 6, 99, 99, 7]) → 5
+# sum67([1, 1, 6, 7, 2]) → 4
+
+# The first code is implemented using the while loop (drawback is the complexity)
+def skip67(nums):
+    total = 0
+    i = 0
+    flag = True
+    while i<len(nums):
+        if nums[i] == 6:
+            while nums[i]!=7:
+                i=i+1
+            i=i+1
+        else:
+            total = total+nums[i]
+            i=i+1
+    return total
+
+print skip67([2,3,4,5,6,13,12,7,2])
+print skip67([1, 2, 2])
+print skip67([1, 2, 2, 6, 99, 99, 7])
+print skip67([1, 1, 6, 7, 2])
+
+# Function sum67 is implemented using Flag indicator (more efficient as the complexity is O(n))
 
 def sum67(nums):
     record = True
