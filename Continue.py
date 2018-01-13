84d8ca59702d401f5fba8c083a1009533b2a1525
juhis <juhisharma21@gmail.com>
2018-01-13 17:23:24 -0500

    Practice Session

---
+++ Continue.py
@@ -0,0 +1,8 @@
+# Program to show the use of continue statement inside loops
+
+for val in "string":
+    if val == "i":
+        continue    #(to continue to the loop)
+    print(val)
+
+print("The end")
