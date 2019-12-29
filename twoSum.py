ed0865b7b882f9d36506cc8b41cde5d0df49ecfc
Juhi Sharma <juhisharma21@gmail.com>
2019-12-29 16:15:48 -0500

    Create twoSum.py

---
+++ twoSum.py
@@ -0,0 +1,22 @@
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+# 
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# 
+# Example:
+# 
+# Given nums = [2, 7, 11, 15], target = 9,
+# 
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+
+def twoSum(nums, target):
+    result = []
+    for i in range(len(nums)):
+        if (target - nums[i] in nums):
+            pos = nums.index(target - nums[i])
+            if pos!=i:
+                result = [i,pos]
+                return "Two index in nums are " + str(result)
+
+
+print(twoSum([2,3,4], 7))
