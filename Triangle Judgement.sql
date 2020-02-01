bc70adbc7d8dbfe3a9c3722e42ecb14858ee5cd5
Juhi Sharma <juhisharma21@gmail.com>
2020-01-31 22:21:46 -0500

    Create Triangle Judgement.sql

---
+++ Triangle Judgement.sql
@@ -0,0 +1,45 @@
+A pupil Tim gets homework to identify whether three line segments could possibly form a triangle.
+ 
+
+However, this assignment is very heavy because there are hundreds of records to calculate.
+ 
+
+Could you help Tim by writing a query to judge whether these three sides can form a triangle, assuming table triangle holds the length of the three sides x, y and z.
+ 
+
+| x  | y  | z  |
+|----|----|----|
+| 13 | 15 | 30 |
+| 10 | 20 | 15 |
+For the sample data above, your query should return the follow result:
+| x  | y  | z  | triangle |
+|----|----|----|----------|
+| 13 | 15 | 30 | No       |
+| 10 | 20 | 15 | Yes      |
+
+#######################
+
+CREATE TABLE Sales.triangle(
+        x int,
+        y int,
+        z int
+    );
+INSERT INTO Sales.triangle (x, y, z) VALUES (13, 15, 30);
+INSERT INTO Sales.triangle(x, y, z) VALUES (10, 20, 15);
+
+Select * from Sales.triangle;
+
+#################
+
+Select x, y, z, 
+case 
+when x+y > z and y+z > x and x+y > z then "Yes" 
+else "No"
+end as triangle
+from triangle;
+
+############## another way ##########
+
+select *, 
+    IF(x + y > z AND x + z > y AND y + z > x, 'Yes', 'No') as triangle 
+    from triangle;
