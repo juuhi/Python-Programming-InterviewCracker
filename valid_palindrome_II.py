b282603778d4db4efe0c8a2008247b4f9ae6e300
Juhi Sharma <juhisharma21@gmail.com>
2020-02-03 11:32:57 -0500

    Update valid_palindrome_II.py

--- valid_palindrome_II.py
+++ valid_palindrome_II.py
@@ -24,3 +24,26 @@ class Solution:
                 return one == one[::-1] or two == two[::-1]
             i, j = i+1, j-1
         return True
+    
+# If it is being asked to remove that element and then print the palindrome ###### then there will be slight modification in
+# the code
+
+def validPalindrome(s):
+    i, j = 0, len(s) - 1
+    while i < j:
+        if s[i] != s[j]:
+            one, two = s[i:j], s[i + 1:j + 1]
+            print(one,two)
+            if (one == one[::-1] or two == two[::-1]):
+                s = s.replace(s[i],'')
+                print(s)
+                return True
+            else:
+                return False
+        i, j = i + 1, j - 1
+    return True
+
+
+#print(validPalindrome('abcedba'))
+#print(validPalindrome('abcdba'))
+print(validPalindrome('abcba'))
