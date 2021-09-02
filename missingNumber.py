860566dd3df960fc01ebafb77b4cce3fea697ef6
Juhi Sharma <juhisharma21@gmail.com>
2021-09-01 23:34:54 -0700

    Update missingNumber.py

--- missingNumber.py
+++ missingNumber.py
@@ -1,1 +1,13 @@
+def MissingNumber(self, array, n):
+        total = (n)*(n+1)//2
+        suma = sum(array)
+        val = total - suma
+        return val
+     
+#  If n is not given in argument of function 
 
+def getMissingNo(A):
+    n = len(A)
+    total = (n + 1)*(n + 2)/2
+    sum_of_A = sum(A)
+    return total - sum_of_A
