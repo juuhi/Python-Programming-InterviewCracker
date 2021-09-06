c43cee13f0946c62741bbd942926377127820d47
Juhi Sharma <juhisharma21@gmail.com>
2021-09-05 19:28:19 -0700

    Update Check_IP_Address.py

--- Check_IP_Address.py
+++ Check_IP_Address.py
@@ -1,13 +1,25 @@
-"""Check that the entered IP address is valid or not
-Condition 1 : It should be in the form of XXX-XXX-XXX-XXX
-XXX should be in between 0 and 255"""
-def Check_IP_Address(String):
-    String = String.split('.')
-    for (i) in String:
-        if not i.isdigit():
-            return False
-        if (len(i)!= 4 or int(i) > 255 or int(i) <0):
-            return False
-    return True
+#Check for valid IPv4 and IPv6 else return Neither
+class Solution:
+    def validIPAddress(self, IP: str) -> str:
+        res = 0
+        ipv4 = IP.split('.')
+        if len(ipv4) == 4:
+            for x in ipv4:
+                if x == '' or (x[0] == '0' and len(x) != 1) or not x.isdigit() or int(x) > 255:
+                    res = 1
+                    break
+            if not res:
+                return 'IPv4'
+        
+        ipv6 = IP.split()
+        if len(ipv6) == 8:
+            for x in ipv6:
+                if x == '' or len(x) > 4 or not all(i in hexdigits for i in x):
+                    res = 1
+                    break
+            if not res:
+                return 'IPv6'
+                    
+        return 'Neither'
 
 print(Check_IP_Address('An.123.122.111'))
