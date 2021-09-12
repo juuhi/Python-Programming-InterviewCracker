95d43ff9f9f88086f269b50d7d7d6d08b4f4540c
Juhi Sharma <juhisharma21@gmail.com>
2021-09-12 15:40:20 -0700

    Update missingNumber.py

--- missingNumber.py
+++ missingNumber.py
@@ -1,4 +1,19 @@
-def MissingNumber(self, array, n):
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+
+ 
+
+Example 1:
+
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+##################################################
+
+def MissingNumber(self, array):
+        n = len(array)
         total = (n)*(n+1)//2
         suma = sum(array)
         val = total - suma
@@ -11,3 +26,4 @@ def getMissingNo(A):
     total = (n + 1)*(n + 2)/2
     sum_of_A = sum(A)
     return total - sum_of_A
+
