707697a82d0d9459d2f76b89420863c6f5885bd2
Juhi Sharma <juhisharma21@gmail.com>
2021-09-16 20:34:42 -0700

    Create FindingUserPurchases.sql

---
+++ FindingUserPurchases.sql
@@ -0,0 +1,2 @@
+Write a query that'll identify returning active users. 
+A returning active user is a user that has made a second purchase within 7 days of any other of their purchases. Output a list of user_ids of these returning active users.
